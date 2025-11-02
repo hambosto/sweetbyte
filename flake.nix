8177b42e640c4fe8ed31951d4a8a871451de20ce
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2025-11-02 22:03:41 +0700

    build(nix): integrate gomod2nix for go module management
    Adopt gomod2nix for declarative Go module management.
    This replaces the manual vendorHash with a gomod2nix.toml
    file and switches to buildGoApplication.
    Also adds a development shell.

--- flake.nix
+++ flake.nix
@@ -1,53 +1,58 @@
 {
   description = "SweetByte - A very small, very simple, yet very secure encryption tool.";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix.url = "github:nix-community/gomod2nix";
+    gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
+    gomod2nix.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
+    inputs@{ ... }:
+    inputs.flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [
+          inputs.gomod2nix.overlays.default
+        ];
+        pkgs = import inputs.nixpkgs {
+          inherit system overlays;
+        };
       in
       {
-        packages.default = pkgs.buildGoModule {
+        packages.default = pkgs.buildGoApplication {
           pname = "sweetbyte";
           version = "1.0";
-
           src = ./.;
-
-          vendorHash = "sha256-ZqjjU4RAU5R8EbhvNiXHyahWmIvY5RvUTQ7CJ7h0bTA=";
-
-          env.CGO_ENABLED = 0;
-          flags = [ "-trimpath" ];
-          ldflags = [
-            "-s"
-            "-w"
-            "-extldflags -static"
-          ];
-
+          modules = ./gomod2nix.toml;
+          # env.CGO_ENABLED = 0;
+          # flags = [ "-trimpath" ];
+          # ldflags = [
+          #   "-s"
+          #   "-w"
+          #   "-extldflags -static"
+          # ];
           nativeBuildInputs = [ pkgs.installShellFiles ];
-
           postInstall = ''
             installShellCompletion --cmd sweetbyte \
               --bash <($out/bin/sweetbyte completion bash) \
               --fish <($out/bin/sweetbyte completion fish) \
               --zsh <($out/bin/sweetbyte completion zsh)
           '';
-
           meta = with pkgs.lib; {
             description = "A very small, very simple, yet very secure encryption tool.";
             homepage = "https://github.com/hambosto/sweetbyte";
             license = licenses.mit;
             mainProgram = "sweetbyte";
+          };
+        };
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gomod2nix
+            ];
           };
         };
       }
